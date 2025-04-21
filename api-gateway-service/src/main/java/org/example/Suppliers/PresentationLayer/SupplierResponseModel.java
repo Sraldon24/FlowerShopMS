d48aea3df6b1612fc2bd9329852e5f999fe64b1a
Mohamed Lhamidi <125913647+it-moh22@users.noreply.github.com>
2025-04-21 19:28:03 -0400

    Payment and Supplier With in the Api Gateway work but not with Hatoes Empty links For both

--- api-gateway-service/src/main/java/org/example/Suppliers/PresentationLayer/SupplierResponseModel.java
+++ api-gateway-service/src/main/java/org/example/Suppliers/PresentationLayer/SupplierResponseModel.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
 
 import java.util.List;
 
@@ -12,7 +13,7 @@ import java.util.List;
 @AllArgsConstructor
 @Getter
 @Setter
-public class SupplierResponseModel  {
+public class SupplierResponseModel extends RepresentationModel<SupplierResponseModel> {
 
     private String supplierId;
     private String companyName;
